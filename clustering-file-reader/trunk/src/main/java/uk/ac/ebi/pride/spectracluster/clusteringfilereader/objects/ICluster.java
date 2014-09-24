ff7162f34ab8aedf802629d5a5df9bf53d12f502
jgriss8@yahoo.com <jgriss8@yahoo.com@332a39bd-9e54-5415-0f82-69358dc792e7>
2014-09-24 12:51:08 +0000

    jg - added a helper function to ClusteringFileCluster to retrieve PSM sequence counts.

--- clustering-file-reader/trunk/src/main/java/uk/ac/ebi/pride/spectracluster/clusteringfilereader/objects/ICluster.java
+++ clustering-file-reader/trunk/src/main/java/uk/ac/ebi/pride/spectracluster/clusteringfilereader/objects/ICluster.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.pride.spectracluster.clusteringfilereader.objects;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -17,7 +18,21 @@ public interface ICluster {
 
     public int getSpecCount();
 
+    /**
+     * Returns the total number of PSMs. This can be larger than the total
+     * number of spectra as spectra can be identified as multiple peptides.
+     * @return
+     */
+    public int getPsmCount();
+
     public List<SequenceCount> getSequenceCounts();
+
+    /**
+     * Returns a Map with the PSM sequence as key and its occurrence
+     * as value.
+     * @return
+     */
+    public Map<String, Integer> getPsmSequenceCounts();
 
     public float getMaxRatio();
 
@@ -28,4 +43,6 @@ public interface ICluster {
     public List<Float> getConsensusMzValues();
 
     public List<Float> getConsensusIntensValues();
+
+
 }
