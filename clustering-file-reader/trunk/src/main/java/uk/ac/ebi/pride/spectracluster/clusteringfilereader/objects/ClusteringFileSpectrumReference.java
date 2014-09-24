d478cf06328aa92b76de68c8174b880b873db035
jgriss8@yahoo.com <jgriss8@yahoo.com@332a39bd-9e54-5415-0f82-69358dc792e7>
2014-09-24 12:22:39 +0000

    jg - added helper functions to clustering file reader to process multiple PSMs with multiple mods

--- clustering-file-reader/trunk/src/main/java/uk/ac/ebi/pride/spectracluster/clusteringfilereader/objects/ClusteringFileSpectrumReference.java
+++ clustering-file-reader/trunk/src/main/java/uk/ac/ebi/pride/spectracluster/clusteringfilereader/objects/ClusteringFileSpectrumReference.java
@@ -1,5 +1,9 @@
 package uk.ac.ebi.pride.spectracluster.clusteringfilereader.objects;
 
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by jg on 01.08.14.
  */
@@ -13,6 +17,8 @@ public class ClusteringFileSpectrumReference implements ISpectrumReference {
     public final int INDEX_SIMILARITY_SCORE = 8;
     public final String SEPARATOR = "\t";
 
+    private final Pattern modificationPattern = Pattern.compile("([0-9]+)-([A-Z:0-9]+),?");
+
     private final String sequence;
     private final int charge;
     private final float precursorMz;
@@ -20,6 +26,9 @@ public class ClusteringFileSpectrumReference implements ISpectrumReference {
     private float similarityScore = 0;
     private final String species;
     private final String modifications;
+
+    private List<IPeptideSpectrumMatch> psms = new ArrayList<IPeptideSpectrumMatch>();
+    private IPeptideSpectrumMatch mostCommonPsm;
 
     public ClusteringFileSpectrumReference(String sequence, int charge, float precursorMz,
                                            String id, float similarityScore, String species,
@@ -31,6 +40,8 @@ public class ClusteringFileSpectrumReference implements ISpectrumReference {
         this.similarityScore = similarityScore;
         this.species = species;
         this.modifications = modifications;
+
+        createPSMs();
     }
 
     public ClusteringFileSpectrumReference(String specLine) throws Exception {
@@ -71,16 +82,70 @@ public class ClusteringFileSpectrumReference implements ISpectrumReference {
         } else {
             similarityScore = 0;
         }
+
+        createPSMs();
+    }
+
+    /**
+     * Creates the PSM objects based on the sequence string and the modification strings.
+     */
+    private void createPSMs() {
+        psms = new ArrayList<IPeptideSpectrumMatch>();
+
+        if (sequence.equals(""))
+            return;
+
+        String[] sequences = sequence.split(",", -1);
+        String[] modificationsPerPSM = (modifications != null) ? modifications.split(";", -1) : null;
+
+        if (modificationsPerPSM != null && sequences.length != modificationsPerPSM.length) {
+            throw new IllegalStateException("Different number of peptide sequences and modification definitions encountered.");
+        }
+
+        for (int i = 0; i < sequences.length; i++) {
+            ClusteringFilePSM psm = new ClusteringFilePSM(sequences[i]);
+
+            if (modificationsPerPSM != null) {
+                Matcher matcher = modificationPattern.matcher(modificationsPerPSM[i]);
+
+                while (matcher.find()) {
+                    int position = Integer.parseInt(matcher.group(1));
+                    String accession = matcher.group(2);
+
+                    ClusteringFileModification mod = new ClusteringFileModification(position, accession);
+                    psm.addModification(mod);
+                }
+            }
+
+            psms.add(psm);
+        }
+
+        // count how often a certain PSM was found
+        Map<ClusteringFilePSM, Integer> psmCounts = new HashMap<ClusteringFilePSM, Integer>();
+
+        for (IPeptideSpectrumMatch psmI : psms) {
+            ClusteringFilePSM psm = (ClusteringFilePSM) psmI;
+
+            if (!psmCounts.containsKey(psm))
+                psmCounts.put(psm, 0);
+
+            psmCounts.put(psm, psmCounts.get(psm) + 1);
+        }
+
+        int maxCount = 0;
+        mostCommonPsm = null;
+
+        for (ClusteringFilePSM psm : psmCounts.keySet()) {
+            if (psmCounts.get(psm) > maxCount) {
+                mostCommonPsm = psm;
+                maxCount = psmCounts.get(psm);
+            }
+        }
     }
 
     @Override
     public String getSpectrumId() {
         return id;
-    }
-
-    @Override
-    public String getSequence() {
-        return sequence;
     }
 
     @Override
@@ -104,9 +169,17 @@ public class ClusteringFileSpectrumReference implements ISpectrumReference {
     }
 
     @Override
-    public String getModifications() {
-        return modifications;
+    public boolean isIdentifiedAsMultiplePeptides() {
+        return psms.size() > 1;
     }
 
+    @Override
+    public IPeptideSpectrumMatch getMostCommonPSM() {
+        return mostCommonPsm;
+    }
 
+    @Override
+    public List<IPeptideSpectrumMatch> getPSMs() {
+        return Collections.unmodifiableList(psms);
+    }
 }
